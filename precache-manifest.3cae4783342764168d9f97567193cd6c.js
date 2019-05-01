e845d3a21e17c83f402895e1dd42efc652ca3b63
Ga Shven <shvendala@gmail.com>
2019-05-01 17:55:08 +0700

    Updates

---
+++ precache-manifest.3cae4783342764168d9f97567193cd6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d07345d114c2d1315264",
+    "url": "/demo/static/css/main.053aaaf5.chunk.css"
+  },
+  {
+    "revision": "d07345d114c2d1315264",
+    "url": "/demo/static/js/main.69f070a1.chunk.js"
+  },
+  {
+    "revision": "b7e673b52c1f3a53650e",
+    "url": "/demo/static/js/runtime~main.4eca427f.js"
+  },
+  {
+    "revision": "36289040b3b10e83db4a",
+    "url": "/demo/static/js/2.fbff5525.chunk.js"
+  },
+  {
+    "revision": "27fd1435c639c5dc8d015d8587d0774f",
+    "url": "/demo/index.html"
+  }
+];
